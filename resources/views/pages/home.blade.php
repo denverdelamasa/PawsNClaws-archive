a94b3ddbf3992e7ffa41a51a20ad63a09236c497
Skibidi <denver.delamasa@gmail.com>
2024-12-07 00:19:00 +0800

    Sidebar complete

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -18,8 +18,19 @@
         <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
         <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
           <!-- Sidebar content here -->
-          <li><a>Sidebar Item 1</a></li>
-          <li><a>Sidebar Item 2</a></li>
+          @auth
+            <upload-post></upload-post>
+          @endauth
+          <ul class="menu bg-base-100 border border-base-300 text-base-content p-4 my-2 rounded-xl">
+            <h2>Shelters near you:</h2>
+            <li><a>Registered Shelter</a></li>
+            <li><a>Registered Shelter</a></li>
+          </ul>
+          <ul class="menu bg-base-100 border border-base-300 text-base-content p-4 my-2 rounded-xl">
+            <h2>Shelters you may Like</h2>
+            <li><a>Registered Shelter</a></li>
+            <li><a>Registered Shelter</a></li>
+          </ul>
         </ul>
       </div>
   </div>
