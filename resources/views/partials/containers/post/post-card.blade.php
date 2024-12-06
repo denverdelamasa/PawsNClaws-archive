85de0efe8fd4252c406aeabb1ed1d909f625ec95
Skibidi <denver.delamasa@gmail.com>
2024-12-06 23:54:14 +0800

    Changed Upvote/DownVote to likes

--- resources/views/partials/containers/post/post-card.blade.php
+++ resources/views/partials/containers/post/post-card.blade.php
@@ -86,12 +86,21 @@
     <!-- Buttons -->
     <div class="flex gap-2 flex-wrap">
       <!-- Upvote Button -->
-      <button class="btn bg-success btn-sm">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="h-5 w-5 stroke-current">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10V21H10V10L5.5 10L12 3.5L18.5 10H14Z" />
+      <button class="btn bg-primary btn-sm text-base-300">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
         </svg>
         <p>4.5k</p>
-        Upvotes
+        Likes
       </button>
 
       <!-- Comments Button -->
