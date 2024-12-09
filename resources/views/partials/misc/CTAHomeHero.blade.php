ff8848d49e90a299dfd6b328f06fc5f4177d6193
Skibidi <denver.delamasa@gmail.com>
2024-12-09 20:19:05 +0800

    Skibidi landing page

---
+++ resources/views/partials/misc/CTAHomeHero.blade.php
@@ -0,0 +1,325 @@
+<div class="flex flex-row gap-x-10 w-full p-auto justify-center">
+  <!-- From Uiverse.io by MuhammadHasann TY SA BUTTONS OMG! -->
+  <button class="GetStartedButton">
+    Get Started
+    <div class="star-1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xml:space="preserve"
+        version="1.1"
+        style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+        viewBox="0 0 784.11 815.53"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+      >
+        <defs></defs>
+        <g id="Layer_x0020_1">
+          <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+          <path
+            class="fil0"
+            d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+          ></path>
+        </g>
+      </svg>
+    </div>
+    <div class="star-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xml:space="preserve"
+        version="1.1"
+        style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+        viewBox="0 0 784.11 815.53"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+      >
+        <defs></defs>
+        <g id="Layer_x0020_1">
+          <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+          <path
+            class="fil0"
+            d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+          ></path>
+        </g>
+      </svg>
+    </div>
+    <div class="star-3">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xml:space="preserve"
+        version="1.1"
+        style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+        viewBox="0 0 784.11 815.53"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+      >
+        <defs></defs>
+        <g id="Layer_x0020_1">
+          <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+          <path
+            class="fil0"
+            d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+          ></path>
+        </g>
+      </svg>
+    </div>
+    <div class="star-4">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xml:space="preserve"
+        version="1.1"
+        style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+        viewBox="0 0 784.11 815.53"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+      >
+        <defs></defs>
+        <g id="Layer_x0020_1">
+          <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+          <path
+            class="fil0"
+            d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+          ></path>
+        </g>
+      </svg>
+    </div>
+    <div class="star-5">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xml:space="preserve"
+        version="1.1"
+        style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+        viewBox="0 0 784.11 815.53"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+      >
+        <defs></defs>
+        <g id="Layer_x0020_1">
+          <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+          <path
+            class="fil0"
+            d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+          ></path>
+        </g>
+      </svg>
+    </div>
+    <div class="star-6">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xml:space="preserve"
+        version="1.1"
+        style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+        viewBox="0 0 784.11 815.53"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+      >
+        <defs></defs>
+        <g id="Layer_x0020_1">
+          <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+          <path
+            class="fil0"
+            d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+          ></path>
+        </g>
+      </svg>
+    </div>
+  </button>
+  <!-- From Uiverse.io by WhiteNervosa TY SA BUTTONS OMG! -->
+  <button class="LearnMoreButton flex flex-row gap-2 align-middle justify-center items-center">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-info-square-fill h-4 w-4" viewBox="0 0 16 16">
+      <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.93 4.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM8 5.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+    </svg>
+      Learn More
+  </button>
+</div>
+<style>
+    /* From Uiverse.io by MuhammadHasann */ 
+    .GetStartedButton {
+      position: relative;
+      padding: 8px 33px;
+      background: #fec195;
+      font-size: 17px;
+      font-weight: 600;
+      color: #181818;
+      border: 3px solid #fec195;
+      border-radius: 8px;
+      box-shadow: 0 0 0 #fec1958c;
+      transition: all 0.3s ease-in-out;
+      cursor: pointer;
+    }
+
+    .star-1 {
+    position: absolute;
+    top: 20%;
+    left: 20%;
+    width: 25px;
+    height: auto;
+    filter: drop-shadow(0 0 0 #fffdef);
+    z-index: -5;
+    transition: all 1s cubic-bezier(0.05, 0.83, 0.43, 0.96);
+    }
+
+    .star-2 {
+    position: absolute;
+    top: 45%;
+    left: 45%;
+    width: 15px;
+    height: auto;
+    filter: drop-shadow(0 0 0 #fffdef);
+    z-index: -5;
+    transition: all 1s cubic-bezier(0, 0.4, 0, 1.01);
+    }
+
+    .star-3 {
+    position: absolute;
+    top: 40%;
+    left: 40%;
+    width: 5px;
+    height: auto;
+    filter: drop-shadow(0 0 0 #fffdef);
+    z-index: -5;
+    transition: all 1s cubic-bezier(0, 0.4, 0, 1.01);
+    }
+
+    .star-4 {
+    position: absolute;
+    top: 20%;
+    left: 40%;
+    width: 8px;
+    height: auto;
+    filter: drop-shadow(0 0 0 #fffdef);
+    z-index: -5;
+    transition: all 0.8s cubic-bezier(0, 0.4, 0, 1.01);
+    }
+
+    .star-5 {
+    position: absolute;
+    top: 25%;
+    left: 45%;
+    width: 15px;
+    height: auto;
+    filter: drop-shadow(0 0 0 #fffdef);
+    z-index: -5;
+    transition: all 0.6s cubic-bezier(0, 0.4, 0, 1.01);
+    }
+
+    .star-6 {
+    position: absolute;
+    top: 5%;
+    left: 50%;
+    width: 5px;
+    height: auto;
+    filter: drop-shadow(0 0 0 #fffdef);
+    z-index: -5;
+    transition: all 0.8s ease;
+    }
+
+    .GetStartedButton:hover {
+      background: transparent;
+      color: #fec195;
+      box-shadow: 0 0 25px #fec1958c;
+      transform: scale(1.1);
+      transition: ease-out 0.2s;
+    }
+
+    .GetStartedButton:hover .star-1 {
+    position: absolute;
+    top: -80%;
+    left: -30%;
+    width: 25px;
+    height: auto;
+    filter: drop-shadow(0 0 10px #fffdef);
+    z-index: 2;
+    }
+
+    .GetStartedButton:hover .star-2 {
+    position: absolute;
+    top: -25%;
+    left: 10%;
+    width: 15px;
+    height: auto;
+    filter: drop-shadow(0 0 10px #fffdef);
+    z-index: 2;
+    }
+
+    .GetStartedButton:hover .star-3 {
+    position: absolute;
+    top: 55%;
+    left: 25%;
+    width: 5px;
+    height: auto;
+    filter: drop-shadow(0 0 10px #fffdef);
+    z-index: 2;
+    }
+
+    .GetStartedButton:hover .star-4 {
+    position: absolute;
+    top: 30%;
+    left: 80%;
+    width: 8px;
+    height: auto;
+    filter: drop-shadow(0 0 10px #fffdef);
+    z-index: 2;
+    }
+
+    .GetStartedButton:hover .star-5 {
+    position: absolute;
+    top: 25%;
+    left: 115%;
+    width: 15px;
+    height: auto;
+    filter: drop-shadow(0 0 10px #fffdef);
+    z-index: 2;
+    }
+
+    .GetStartedButton:hover .star-6 {
+    position: absolute;
+    top: 5%;
+    left: 60%;
+    width: 5px;
+    height: auto;
+    filter: drop-shadow(0 0 10px #fffdef);
+    z-index: 2;
+    }
+
+    .fil0 {
+      fill: #fffdef;
+    }
+
+
+    /* From Uiverse.io by WhiteNervosa */ 
+    .LearnMoreButton {
+      font-size: 18px;
+      color: #e1e1e1;
+      font-family: inherit;
+      font-weight: 800;
+      cursor: pointer;
+      position: relative;
+      border: none;
+      background: none;
+      text-transform: uppercase;
+      transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
+      transition-duration: 400ms;
+      transition-property: color;
+    }
+
+    .LearnMoreButton:focus,
+    .LearnMoreButton:hover {
+      color: #fff;
+      transform: scale(1.1);
+      transition: ease-out 0.2s;
+    }
+
+    .LearnMoreButton:focus:after,
+    .LearnMoreButton:hover:after {
+      width: 100%;
+      left: 0%;
+    }
+
+    .LearnMoreButton:after {
+      content: "";
+      pointer-events: none;
+      bottom: -2px;
+      left: 50%;
+      position: absolute;
+      width: 0%;
+      height: 2px;
+      background-color: #fff;
+      transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
+      transition-duration: 400ms;
+      transition-property: width, left;
+    }
+</style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,92 @@
     <body class="font-sans antialiased">
         @include('partials.main.navbar')
       
-        <div id="app2">
+        <section
+            class="hero min-h-[70vh]"
+            style="
+            background-image: url('{{ asset('images/pictures/pexels-wildlittlethingsphoto.jpg') }}');
+            background-size: cover;
+            background-position: center;
+            "
+            id="parallaxHero">
+            <div class="hero-overlay bg-opacity-60"></div>
+            <div class="hero-content text-neutral-content text-center">
+                <div class="max-w-full">
+                    <h1 class="mb-5 text-8xl font-bold">Discover, Share, and Connect – All in One Place!</h1>
+                    <p class="mb-5">
+                        Join a thriving community of pet lovers. Share your stories, find your next furry friend, or connect with shelters near you!
+                    </p>
+                    @include('partials.misc.CTAHomeHero')
+                </div>
+            </div>
+        </section>
+      
+        <section class="h-[80vh] text-base-content body-font align-middle items-center justify-center m-auto flex">
+            <div class="container flex flex-row w-full h-full"> <!-- Make sure the container stretches fully -->
+              <div class="flex flex-col text-start justify-start w-1/2 m-auto p-2">
+                <h1 class="sm:text-5xl text-2xl font-medium title-font text-primary">Why Become Part of Our Community?</h1>
+                <p class="leading-relaxed text-base text-secondary">
+                    Discover a platform designed to connect pet lovers, shelters, and fosters. Share experiences, find new furry friends, and access exclusive resources that make a real difference in the world of pet adoption and care.
+                </p>
+              </div>
+              <div class="flex flex-wrap w-1/2 m-auto gap-2"> <!-- Add gap property here -->
+                <div class="w-full">
+                  <div class="w-full h-[300px] rounded-xl bg-base-300 shadow-lg"></div> <!-- A box replacing the image -->
+                </div>
+                <div class="flex flex-row w-full gap-2">
+                    <div class="w-1/2">
+                      <div class="w-full h-[150px] rounded-xl bg-base-300 shadow-lg"></div> <!-- A box replacing the image -->
+                    </div>
+                    <div class="w-1/2">
+                      <div class="w-full h-[150px] rounded-xl bg-base-300 shadow-lg"></div> <!-- A box replacing the image -->
+                    </div>
+                </div>
+              </div>                           
+            </div>
+        </section>    
+          
+        <section class="h-[80vh] w-full bg-base-300">
+        </section>
+        <section class="flex h-[80vh] bg-base-100 text-base-content">
+            <div class="container m-auto">
+              <h1 class="text-5xl font-medium title-font text-base-content mb-12 text-center">Testimonials</h1>
+              <div class="flex flex-wrap">
+                <div class="p-4 md:w-1/2 w-full">
+                  <div class="h-full bg-base-200 p-8 rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="block w-5 h-5 text-base-content/50 mb-4" viewBox="0 0 975.036 975.036">
+                      <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                    </svg>
+                    <p class="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
+                    <a class="inline-flex items-center">
+                      <img alt="testimonial" src="https://dummyimage.com/106x106" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">
+                      <span class="flex-grow flex flex-col pl-4">
+                        <span class="title-font font-medium text-base-content">Holden Caulfield</span>
+                        <span class="text-base-content/60 text-sm">UI DEVELOPER</span>
+                      </span>
+                    </a>
+                  </div>
+                </div>
+                <div class="p-4 md:w-1/2 w-full">
+                  <div class="h-full bg-base-200 p-8 rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="block w-5 h-5 text-base-content/50 mb-4" viewBox="0 0 975.036 975.036">
+                      <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                    </svg>
+                    <p class="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
+                    <a class="inline-flex items-center">
+                      <img alt="testimonial" src="https://dummyimage.com/107x107" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">
+                      <span class="flex-grow flex flex-col pl-4">
+                        <span class="title-font font-medium text-base-content">Alper Kamu</span>
+                        <span class="text-base-content/60 text-sm">DESIGNER</span>
+                      </span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </section>
 
-        </div>
+
+
     </body>
 
     <footer class="footer bg-base-300 text-base-content p-4 text-xs">
@@ -108,5 +191,18 @@
         transform: translate(20%, 20%);
     }
     }
+    #parallaxHero {
+    background-attachment: scroll;
+    background-position: center;
+    transition: background-position 0.1s ease-out;
+  }
 
 </style>
+<script>
+    const hero = document.getElementById("parallaxHero");
+  
+    window.addEventListener("scroll", () => {
+      const scrollPosition = window.scrollY;
+      hero.style.backgroundPosition = `center ${scrollPosition * 0.5}px`;
+    });
+</script>
