261ab25e351a0cef3e309f5c11d65c6436135f62
Meebly <denver.delamasa@gmail.com>
2024-12-09 15:16:23 +0800

    Dec 12, 2024 | Nag present kay sir pau minor fix

--- resources/js/components/partials/posts/PostCard.vue
+++ resources/js/components/partials/posts/PostCard.vue
@@ -275,19 +275,20 @@
       </button>
 
       <!-- Apply Adopt Button -->
-      <button v-if="post.is_adoptable === 1, post.user_id !== currentUserId" @click="openAdoptionModal(post.post_id, post.user_id)" class="btn btn-outline btn-success btn-sm flex items-center gap-2">
+      <button v-if="post.is_adoptable === 1 && post.user_id !== currentUserId" @click="openAdoptionModal(post.post_id, post.user_id)" class="btn btn-outline btn-success btn-sm flex items-center gap-2">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-paper-heart" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v1.133l.941.502A2 2 0 0 1 16 5.4V14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5.4a2 2 0 0 1 1.059-1.765L2 3.133zm0 2.267-.47.25A1 1 0 0 0 1 5.4v.817l1 .6zm1 3.15 3.75 2.25L8 8.917l1.25.75L13 7.417V2a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1zm11-.6 1-.6V5.4a1 1 0 0 0-.53-.882L14 4.267zM8 2.982C9.664 1.309 13.825 4.236 8 8 2.175 4.236 6.336 1.31 8 2.982m7 4.401-4.778 2.867L15 13.117zm-.035 6.88L8 10.082l-6.965 4.18A1 1 0 0 0 2 15h12a1 1 0 0 0 .965-.738ZM1 13.116l4.778-2.867L1 7.383v5.734Z"/>
         </svg>
         <span>Send Adoption Form</span>
       </button>
-      <!-- Apply Adopt Button -->
-      <button class="btn btn-outline btn-active btn-warning btn-sm flex items-center gap-2">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-paper-heart-fill" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="m3 7.5 3.5 2L8 8.75l1.5.75 3.5-2v-6A1.5 1.5 0 0 0 11.5 0h-7A1.5 1.5 0 0 0 3 1.5zM2 3.133l-.941.502A2 2 0 0 0 0 5.4v.313l2 1.173zm12 3.753 2-1.173V5.4a2 2 0 0 0-1.059-1.765L14 3.133zm-3.693 3.324L16 6.873v6.5zm5.634 4.274L8 10.072.059 14.484A2 2 0 0 0 2 16h12a2 2 0 0 0 1.941-1.516M5.693 10.21 0 13.372v-6.5zM8 1.982C9.664.309 13.825 3.236 8 7 2.175 3.236 6.336.31 8 1.982"/>
-        </svg>
-        <span>Done Sending Adoption Form</span>
-      </button>
+      <!-- Apply Adopt Button 
+        <button class="btn btn-outline btn-active btn-warning btn-sm flex items-center gap-2">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-paper-heart-fill" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="m3 7.5 3.5 2L8 8.75l1.5.75 3.5-2v-6A1.5 1.5 0 0 0 11.5 0h-7A1.5 1.5 0 0 0 3 1.5zM2 3.133l-.941.502A2 2 0 0 0 0 5.4v.313l2 1.173zm12 3.753 2-1.173V5.4a2 2 0 0 0-1.059-1.765L14 3.133zm-3.693 3.324L16 6.873v6.5zm5.634 4.274L8 10.072.059 14.484A2 2 0 0 0 2 16h12a2 2 0 0 0 1.941-1.516M5.693 10.21 0 13.372v-6.5zM8 1.982C9.664.309 13.825 3.236 8 7 2.175 3.236 6.336.31 8 1.982"/>
+          </svg>
+          <span>Done Sending Adoption Form</span>
+        </button>
+      -->
       <dialog id="adoptionModal" class="modal">
         <div class="modal-box w-96 h-auto max-w-6xl max-h-[80vh] hide-scrollbar">
           <form method="dialog">
