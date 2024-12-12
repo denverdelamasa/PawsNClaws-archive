9aa618066abeef36dc3298493ce72616f5740ee6
Skibidi <denver.delamasa@gmail.com>
2024-12-13 00:57:00 +0800

    Pushing 12:56

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -97,13 +97,13 @@
                         </a></li>
                     </summary>
                     <div class="collapse-content ml-2 flex flex-col gap-y-2">
-                        <li><a href="{{ url('/admin/dashboard/privacy-policy') }}" class="{{ Request::is('admin/dashboard/monetization') ? 'active' : '' }}">
+                        <li><a href="{{ url('/admin/dashboard/privacy-policy') }}" class="{{ Request::is('admin/dashboard/privacy-policy') ? 'active' : '' }}">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-gear w-6 h-6" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.8 11.8 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7 7 0 0 0 1.048-.625 11.8 11.8 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.54 1.54 0 0 0-1.044-1.263 63 63 0 0 0-2.887-.87C9.843.266 8.69 0 8 0m0 5a1.5 1.5 0 0 1 .5 2.915l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99A1.5 1.5 0 0 1 8 5"/>
                             </svg>
                             Privacy Policy
                         </a></li>
-                        <li><a href="{{ url('/admin/dashboard/terms-of-service') }}" class="{{ Request::is('admin/dashboard/monetization') ? 'active' : '' }}">
+                        <li><a href="{{ url('/admin/dashboard/terms-of-service') }}" class="{{ Request::is('admin/dashboard/terms-of-service') ? 'active' : '' }}">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-gear w-6 h-6" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M10.854 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 8.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
                                 <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2"/>
