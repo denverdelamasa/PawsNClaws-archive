c907a403cf5167d91f11988f15017d8b27cd0512
DWEDM <111408088+DWEDM@users.noreply.github.com>
2025-01-09 11:20:58 +0800

    Update Dashboard.vue
    afdsgagsdg

--- resources/js/components/Admin/Dashboard.vue
+++ resources/js/components/Admin/Dashboard.vue
@@ -60,7 +60,7 @@
 <section class="body-font">
     <div class="container px-5 py-24 mx-auto flex justify-center flex-col align-middle items-center">
         <div class="flex flex-col text-center w-full mb-4">
-            <h1 class="sm:text-3xl text-2xl font-medium title-fonttext-primary">Recent Activities</h1>
+            <h1 class="sm:text-3xl text-2xl font-medium title-font text-primary">Recent Activities</h1>
             <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-secondary">
                 Recent logs decription
             </p>
